8c7d65842429e3fa98f9141c4f737b7af3755884
VojtechStep <vojtechstepancik@outlook.com>
2018-11-14 17:00:46 +0100

    Add ACM C++ compilation alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,6 +22,8 @@ abbr -a ghub git clone https://github.com/
 
 abbr -a shutdown shutdown now
 
+abbr -a acmc g++ -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE -o main ./main.cpp
+
 if status --is-login
 	if test -z "$DISPLAY" -a $XDG_VTNR = 1
 		exec startx
