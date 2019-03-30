bf5888edcaddcf472679d3535ae0867e2f22a3a9
VojtechStep <vojtechstepancik@outlook.com>
2019-03-30 20:02:31 +0100

    [system] Add a script to setup touchpad settings

---
+++ touchpad_setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+ID=$(xinput list | grep Touchpad | cut -f2 | cut -d"=" -f2)
+
+xinput --set-prop $ID 'libinput Accel Speed' 0.5
+xinput --set-prop $ID 'libinput Tapping Enabled' 1
+xinput --set-prop $ID 'libinput Natural Scrolling Enabled' 1
+xinput --set-prop $ID 'libinput Middle Emulation Enabled' 1
+
