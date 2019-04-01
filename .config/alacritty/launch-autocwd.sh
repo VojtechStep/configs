4c237054557658ee2977520233843cb30f185104
VojtechStep <vojtechstepancik@outlook.com>
2019-04-01 22:53:07 +0200

    [system] Add an alternate way of detecting working dir for new terminals

--- .config/alacritty/launch-autocwd.sh
+++ .config/alacritty/launch-autocwd.sh
@@ -2,9 +2,11 @@
 
 ID=$(xdpyinfo | grep -m1 focus | cut -d " " -f4)
 PID=$(($(xprop -id $ID | grep -m1 PID | cut -d " " -f3) + 2))
-if [ -e "/proc/$PID/cwd" ]
-then
+ALTERN_WD=$(xprop -id $ID | grep -m1 -w WM_NAME | cut -d= -f2 | tr -d "\"" | rev | cut -d" " -f1 | rev)
+if [ -e "/proc/$PID/cwd" ]; then
 	alacritty --working-directory $(readlink /proc/$PID/cwd)
+elif [ -d "$ALTERN_WD" ]; then
+	alacritty --working-directory $ALTERN_WD
 else
 	alacritty
 fi
