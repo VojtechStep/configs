a17a76880411a0d465f5c3ec9ae2d433cb6564b6
VojtechStep <vojtechstepancik@outlook.com>
2019-04-01 22:56:28 +0200

    [system] Add a script to adjust brightness and bind keyboard shortcuts to it

---
+++ .config/scripts/display_adj.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/sh
+
+INCREMENT=0.05
+CONFIG=$HOME/.brightness
+
+if [ -f "$CONFIG" ]; then
+	LEVEL=$(cat $CONFIG)
+else
+	LEVEL=1
+fi
+
+function adjust {
+	xrandr | rg -w connected | cut -d" " -f1 | xargs -L1 -I{} xrandr --output {} --brightness $LEVEL
+	echo $LEVEL > $CONFIG
+}
+
+case $1 in
+	+)
+		NEW_LEVEL=$(echo "scale=2; $LEVEL + $INCREMENT" | bc)
+		if [ "$(echo "$NEW_LEVEL <= 1" | bc)" -eq 1 ]; then
+			LEVEL=$NEW_LEVEL
+			adjust
+		fi
+		;;
+	-)
+		NEW_LEVEL=$(echo "scale=2; $LEVEL - $INCREMENT" | bc)
+		if [ "$(echo "$NEW_LEVEL >= 0.1" | bc)" -eq 1 ]; then
+			LEVEL=$NEW_LEVEL
+			adjust
+		fi
+		;;
+esac
+
