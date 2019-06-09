664b2ed3aaf0c9baefafc48b115b58c927b1456c
VojtechStep <vojtechstepancik@outlook.com>
2019-06-09 22:28:03 +0200

    [dwm] Add a notification script

---
+++ .config/scripts/notify.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+text=$1
+duration=$2
+
+if [ "$1" = "" ]; then
+	echo "Usage:"
+	echo "	notify [text] [duration]"
+	echo
+	echo "	text"
+	echo "		Text to display"
+	echo "	duration"
+	echo "		For how long to display the text, in seconds"
+	exit 1
+fi
+
+if [ "$duration" = "" ]; then
+	duration=3
+fi
+
+if [ "$(echo "1 * $duration" | bc)" != "$duration" ]; then
+	echo "Unknown duration: $duration"
+	exit 1
+fi
+
+pkill -RTMIN+5 -f "sh .*dwm_status"
+
+xsetroot -name " $text "
+sleep $duration &
+wait $!
+
+pkill -RTMIN+6 -f "sh .*dwm_status"
