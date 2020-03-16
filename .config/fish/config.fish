1e09733c3624c5b7183deeca62ca792074664b7c
VojtechStep <vojtechstepancik@outlook.com>
2020-03-16 23:46:06 +0100

    [system] Switch to autojump-rs
    Drop in replacement, Python -> Rust
    Added shell wrappers from the original project

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,29 +6,42 @@ load-env ~/.envs.el
 
 set -U fish_user_paths
 
-abbr -a v nvim
-abbr -a vim nvim
+abbr -a v $VISUAL
+abbr -a e emacsclient -c
+abbr -a em emacs
+abbr -a rem systemctl --user restart emacs
+abbr -a eaf 'emacsclient --eval "(projectile-add-known-project \""(pwd)"\")"'
+
 
 abbr -a ls exa -l
 abbr -a la exa -la
 
-abbr -a ghub git clone https://github.com/
+abbr -a shitdown systemctl poweroff
+abbr -a shutdown systemctl poweroff
 
-abbr -a shitdown shutdown now
-abbr -a shutdown shutdown now
+abbr -a maek make
 
-abbr -a acmc g++ -lm -lcrypt -O2 -std=c++11 -pipe -DONLINE_JUDGE -o main ./main.cpp
+abbr -a a archey4
 
-abbr -a a archey3
+abbr dr docker run -it --rm
+
+abbr gis git status
+abbr gif git fetch
+abbr gip git pull
+abbr gipl git plog
+abbr gipo git plog origin/master
+
+abbr vf nvim ~/.config/fish/config.fish
+abbr vv nvim ~/.config/nvim/init.vim
 
 if status --is-login
-	if test -z "$DISPLAY" -a $XDG_VTNR = 1
-		exec startx
-	end
+  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+    exec startx
+  end
 end
 
-if test -f $HOME/.autojump/share/autojump/autojump.fish
-	source $HOME/.autojump/share/autojump/autojump.fish
+if test -f ~/.config/fish/autojump.fish
+  source ~/.config/fish/autojump.fish
 end
 
 type nvm >/dev/null ^/dev/null; and nvm use 12
---
+++ .config/fish/functions/jd.fish
@@ -0,0 +1,4 @@
+# Defined in /tmp/fish.c9lXpo/jd.fish @ line 1
+function jd
+	pushd (autojump $argv)
+end
