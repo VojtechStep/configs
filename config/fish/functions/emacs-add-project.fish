d98a0674d0344f9717d0e60a33f6010446ab89cb
VojtechStep <vojtechstepancik@outlook.com>
2022-09-01 19:43:59 +0200

    [fish] Remove dependency on vs-utils.el

--- config/fish/functions/emacs-add-project.fish
+++ config/fish/functions/emacs-add-project.fish
@@ -1,6 +1,7 @@
 function emacs-add-project --argument dir
-  if test -n dir
-    set dir (pwd)
-  end
-  emacsclient -a "" --eval "(ignore (and (require 'vs-utils.rc) (vs/add-project-maybe \"$dir\")))"
+    if test -n dir
+        set dir (pwd)
+    end
+    set dir "\"$dir\""
+    emacsclient -a "" --eval "(ignore (and (projectile-project-p $dir) (projectile-add-known-project $dir)))"
 end
